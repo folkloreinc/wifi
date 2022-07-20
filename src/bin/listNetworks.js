d48e423c71031c78233de8af7e8860c9463f3f66
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2022-07-20 11:53:48 -0400

    Added networks

--- src/bin/listNetworks.js
+++ src/bin/listNetworks.js
@@ -6,16 +6,20 @@ import run from '../utils/run';
 function listNetworks(int) {
     switch (os.platform()) {
         case 'darwin':
-            return run('/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport', ['-s'])
-                .then((out) => {
-                    const table = out.split('\n').map(it => it.trim());
-                    const [columns, ...rows] = table;
-                    console.log(columns.split('\t'));
-                });
-        case 'linux':
-            return run('iw', ['dev']).then((out) => {
-                
+            return run(
+                '/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport',
+                ['-s'],
+            ).then((out) => {
+                const table = out.split('\n').map((it) => it.trim());
+                const [columns, ...rows] = table;
+                console.log(columns.split('\t'));
             });
+        case 'linux':
+            return run('nmcli', ['device', 'wifi', 'rescan', 'ifname', int]).then(() =>
+                run('nmcli', ['device', 'wifi', 'list', 'ifname', int]).then((out) => {
+                    console.log(out);
+                }),
+            );
         default:
             return Promise.reject();
     }
