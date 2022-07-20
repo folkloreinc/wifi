3f2cb06be996b2eb0dbb2611e5d8dc85d684f076
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2022-07-20 11:34:10 -0400

    Added captive portal

--- src/cli/server.js
+++ src/cli/server.js
@@ -7,6 +7,14 @@ command.description('Run server').action(async () => {
     const app = express();
     const port = 5000;
 
+    app.use((req, res, next) => {
+        if (req.get('User-Agent') && req.get('User-Agent').search('CaptiveNetworkSupport') !== -1) {
+            console.log('CAPTIVE');
+            return res.end('NO SUCCESS');
+        }
+        return next();
+    });
+
     app.get('/', (req, res) => {
         res.send('Hello World!');
     });
