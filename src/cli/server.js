184ea104562a197767bf9d8f6fa357473f7b9190
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2022-07-20 11:41:29 -0400

    Added page

--- src/cli/server.js
+++ src/cli/server.js
@@ -1,9 +1,12 @@
 import { Command } from 'commander';
 import express from 'express';
+import path from 'path';
 
 const command = new Command('server');
 
 command.description('Run server').action(async () => {
+    const staticPath = path.join(__dirname, '../public');
+
     const app = express();
     const port = 5000;
 
@@ -15,8 +18,10 @@ command.description('Run server').action(async () => {
         return next();
     });
 
-    app.get('/', (req, res) => {
-        res.send('Hello World!');
+    app.use(express.static(staticPath));
+
+    app.get('*', (req, res) => {
+        return res.sendFile(path.join(staticPath, 'index.html'));
     });
 
     app.listen(port, () => {
