153738a6e29358fccb34c440ddc43c3424bddaee
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2022-07-21 16:36:41 -0400

    Fix

--- babel.config.js
+++ babel.config.js
@@ -13,6 +13,5 @@ module.exports = {
 
     plugins: [[require.resolve('@babel/plugin-transform-runtime'), {}]],
 
-    include: [/src/, /node_modules/],
-    exclude: [],
+    ignore: [/node_modules\/(?!is-online)/],
 };
