0cc5a8926d866c2c19fc6257c64bbd217aad0fe4
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2022-07-21 15:22:22 -0400

    Fix

--- src/bin/getNetwork.js
+++ src/bin/getNetwork.js
@@ -14,19 +14,15 @@ function getNetwork(int) {
                     if (value === null || value.trim().length === 0) {
                         throw new Error('No connection');
                     }
-                    console.log(value);
                     return value.trim();
                 })
-                .then((connectionName) => {
-                    console.log('nmcli', ['conn', 'show', `"${connectionName}"`]);
-                    return run('nmcli', ['conn', 'show', `"${connectionName}"`]);
-                })
+                .then((connectionName) => run('nmcli', ['conn', 'show', `"${connectionName}"`]))
                 .then((out) => {
                     const ssidLine = out
                         .split('\n')
                         .find((line) => line.match(/^802-11-wireless\.ssid/) !== null);
-                        console.log(ssidLine);
                     const [, value = null] = ssidLine.split(':', 2) || [];
+                    console.log(value);
                     return value !== null ? value.trim() : null;
                 })
                 .catch(() => null);
